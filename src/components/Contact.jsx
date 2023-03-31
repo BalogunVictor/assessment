1451a9308b809094e9c0ba62a57531918ea0dc7d
balogunvictor <praisetheultimate@gmail.com>
2023-03-31 10:14:32 +0100

    app UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,68 +5,90 @@ import { BsTelephone } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <div className='w-full h-[800px] bg-[#091242] flex flex-col justify-center items-center text-white'>
-      <div className='flex w-[600px] h-[500px] '>
+
+    <div className='w-full h-[1000px] bg-[#091242] flex flex-col justify-center items-center text-white '>
+      <div className='flex h-[500px]'>
         {/* ////////////////////// */}
-        <div className=''>
+        <div className='w-[400px] '>
           <h2>Get in touch with us</h2>
           <p>Leverage agile frameworks to provide a robust synopsis for strategy foster collaborative thinking to further the overall value.</p>
+            <div className='flex justify-center items-center'>
+            <div className='h-[63px] w-[63px] bg-[#111C55] rounded-full flex justify-center items-center'>
+              <BiEnvelope 
+              size={20}
+              style={{color:'#F6B426'}}
+              />
+            </div>
+            <p className='text-white font-medium text-left px-4'>Email<br/>
+            contact@logistics.com</p>
+          </div>
+
+          <div className='flex justify-center items-center py-4'>
+            <div className='h-[63px] w-[63px] bg-[#111C55] rounded-full flex justify-center items-center'>
+              <BsTelephone
+              size={20}
+              style={{color:'#F6B426'}}
+              />
+            </div>
+            <p className='text-white font-medium text-left px-4'>Call Us 
+            <br/>(00) 112 365 489</p>
+          </div>
+
           <div className='flex justify-center items-center'>
           <div className='h-[63px] w-[63px] bg-[#111C55] rounded-full flex justify-center items-center'>
-            <BiEnvelope 
+            <AiOutlineClockCircle 
             size={20}
             style={{color:'#F6B426'}}
             />
           </div>
-          <p className='text-white font-medium text-left px-4'>Email<br/>
-          contact@logistics.com</p>
+          <p className='text-white font-medium text-left px-4'>Mon - Sat 9.00 - 18.00<br/>Sunday Closed</p>
         </div>
-
-        <div className='flex justify-center items-center'>
-          <div className='h-[63px] w-[63px] bg-[#111C55] rounded-full flex justify-center items-center'>
-            <BsTelephone
-            size={20}
-            style={{color:'#F6B426'}}
-            />
-          </div>
-          <p className='text-white font-medium text-left px-4'>Call Us 
-          
-          <br/>(00) 112 365 489</p>
-        </div>
-
-        <div className='flex justify-center items-center'>
-        <div className='h-[63px] w-[63px] bg-[#111C55] rounded-full flex justify-center items-center'>
-          <AiOutlineClockCircle 
-          size={20}
-          style={{color:'#F6B426'}}
-          />
-        </div>
-        <p className='text-white font-medium text-left px-4'>Mon - Sat 9.00 - 18.00<br/>Sunday Closed</p>
       </div>
-    </div>
+          
+          
 
         {/* ////////////////////// */}
-        <div>
-          <form>
-            <div className='grid grid-cols-2'>
-              <input type="text" placeholder='Your name*' />
-              <input type="text" placeholder='Email*' />
-              <input type="text" placeholder='Phone Number*' />
-              <input type="text" placeholder='City*' />
+        <div className='w-[700px] ml-10  '>
+          <form className='w-[600px]'>
+            <div className='grid grid-cols-2 gap-6'>
+              <input 
+                className='w-[300px] h-14 bg-inherit border-2 border-white' type="text" 
+                placeholder='Your name*' 
+              />
+              <input 
+                className='w-[300px] h-14 bg-inherit border-2 border-white'
+                type="text" 
+                placeholder='Email*'
+               />
+              <input 
+                className='w-[300px] h-14 bg-inherit border-2 border-white' 
+                type="text" 
+                placeholder='Phone Number*' 
+              />
+              <input 
+                className='w-[300px] h-14 bg-inherit border-2 border-white' 
+                type="text" 
+                placeholder='City*' 
+              />
             </div>
-            <input type="text" placeholder='Your Message' />
-            <button>Submit Message</button>
+            <input 
+              className='w-full h-24 bg-inherit border-2 border-white mt-6' 
+              type="text" 
+              placeholder='Your Message'
+             />
+            <button className='w-[194] h-[60px] text-[#111C55] bg-[#FFB629] mt-[30px] p-[19px]'>Submit Message</button>
           </form>
         </div>
-        {/* ////////////////////// */}
-        <div>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
         </div>
-
-      </div>
+        {/* ////////////////////// */}
+        
+        
+        <div className='flex'>
+          <img src="/assests/contact-1.png" alt="/" />
+          <img src="/assests/contact-2.png" alt="/" />
+          <img src="/assests/contact-3.png" alt="/" />
+          <img src="/assests/contact-4.png" alt="/" />
+        </div>
     </div>
   )
 }
